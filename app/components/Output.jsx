145b0519450f5bbaf7a5b9bcb7dc73c2889c40db
John Pinkerton <jpinkerton88@gmail.com>
2016-03-25 23:22:58 -0400

    add complete on copy

--- app/components/Output.jsx
+++ app/components/Output.jsx
@@ -2,20 +2,35 @@ import React from 'react';
 
 export default class Output extends React.Component {
 
+  getSrc(src, options = { alwaysComplete: false }) {
+    if (options.alwaysComplete) return "/cow.png"
+
+    if (src.match(/sam/) === null) {
+      return "/sam.png";
+    } else {
+      return "/cow.png";
+    }
+  }
+
   toggleComplete(event) {
     event.preventDefault();
     let img = event.currentTarget.children[0];
-    if (img.src.match(/sam/) === null) {
-      img.src = "/sam.png";
-    } else {
-      img.src = "/cow.png";
-    }
+    img.src = this.getSrc(img.src);
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", function(e){
+        if (e.keyCode == 67 && (e.ctrlKey || e.metaKey)){
+          let img = e.target.parentElement.children[0].children[0];
+          img.src = this.getSrc(img.src, { alwaysComplete: true });
+        }
+    }.bind(this));
   }
 
   render() {
     return (
       <section style={{padding: "20px 0", position: "relative" }}>
-        <a href="#" onClick={this.toggleComplete} style={{ position: "absolute", right: "0", top: "-10px" }}>
+        <a href="#" onClick={this.toggleComplete.bind(this)} style={{ position: "absolute", right: "0", top: "-10px" }}>
           <img src="/sam.png" />
         </a>
         <h4>Herd #{this.props.chunkNum + 1}</h4>
