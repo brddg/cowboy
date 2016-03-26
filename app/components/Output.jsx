33aee14cfa4e54c4ad30892e0b63726bfe806074
John Pinkerton <jpinkerton88@gmail.com>
2016-03-25 23:03:38 -0400

    add completion status

--- app/components/Output.jsx
+++ app/components/Output.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 export default class Output extends React.Component {
 
+  toggleComplete(event) {
+    event.preventDefault();
+    let img = event.currentTarget.children[0];
+    if (img.src.match(/sam/) === null) {
+      img.src = "/sam.png";
+    } else {
+      img.src = "/cow.png";
+    }
+  }
+
   render() {
     return (
-      <section style={{padding: "20px 0"}}>
+      <section style={{padding: "20px 0", position: "relative" }}>
+        <a href="#" onClick={this.toggleComplete} style={{ position: "absolute", right: "0", top: "-10px" }}>
+          <img src="/sam.png" />
+        </a>
         <h4>Herd #{this.props.chunkNum + 1}</h4>
         <p><small>{this.props.emails.length} chars</small></p>
         <textarea
